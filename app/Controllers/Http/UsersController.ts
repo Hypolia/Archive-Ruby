4f7a083d46ad929b04e72a6bc20828f8c3c3a19e
Nathael Bonnal <kirua48000@gmail.com>
2021-08-10 15:41:30 +0200

    :construction: update method and add destroy async method

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -7,38 +7,39 @@ import UpdateValidator from "App/Validators/users/UpdateValidator";
 
 export default class UsersController {
   public async index() {
-    const users = await User.query().preload('roles', (role) => {
-        role.orderBy('permission_level')
+    return User.query().preload('roles', (role) => {
+      role.orderBy('permission_level')
     })
-    return { users }
   }
 
   public async show({ params }: HttpContextContract) {
-    const user = await User.findOrFail(params.id)
-    await user.preload('roles', (role) => {
-      role.orderBy('permission_level')
+    return User.query().where('id', params.id).preload('roles', (role) => {
+      role.orderBy('permission_level', 'desc')
     })
-    return { user }
   }
 
   public async store({ request }: HttpContextContract) {
-    console.log(request.all())
     const data = await request.validate(StoreValidator)
-    const user = await User.create(data)
 
-    return { user }
+    return await User.create(data)
   }
 
   public async update({ request, params }: HttpContextContract) {
-    const user = await User.findOrFail(params.id)
-    const roles = await request.input('roles')
+    const user = await User.find(params.id)
     const data = await request.validate(UpdateValidator)
 
-    await user.merge(data).save()
-    if (roles) {
-      await user.related('roles').sync(roles)
+    await user?.merge(data).save()
+    if (data.roles) {
+      await user?.related('roles').sync(data.roles)
     }
 
     return { message: 'Le compte a été mis à jour' }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const user = await User.find(params.id)
+    await user?.delete()
+
+    return response.ok("Le compte a été supprimé")
   }
 }
