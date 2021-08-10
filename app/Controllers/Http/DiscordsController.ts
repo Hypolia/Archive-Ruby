3d117dc82c96054715ac4f0702a7a4e1c0b6f629
Nathael Bonnal <kirua48000@gmail.com>
2021-08-10 15:42:01 +0200

    :construction: create async (index/show/store) method

---
+++ app/Controllers/Http/DiscordsController.ts
@@ -0,0 +1,28 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import {HttpContextContract} from "@ioc:Adonis/Core/HttpContext";
+import Discord from "App/Models/Discord";
+import StoreValidator from "App/Validators/discord/StoreValidator";
+
+export default class DiscordsController {
+
+  public async index() {
+    return Discord.all()
+  }
+
+  public async show({ params }: HttpContextContract) {
+    return  Discord.find(params.userId)
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = await request.validate(StoreValidator)
+
+    return await Discord.create(data)
+  }
+
+/*  public async update({ request, params }: HttpContextContract) {
+    const user = await Discord.find(params.id)
+    const data = await request.validate(UpdateValidator)
+
+  }*/
+}
