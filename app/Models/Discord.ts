c1b9320c4c67f511a6818277bead1a03f798cd16
Nathael Bonnal <kirua48000@gmail.com>
2021-08-13 14:29:49 +0200

    :construction: create 3 model account of Hypolia (version alpha)

--- app/Models/Discord.ts
+++ app/Models/Discord.ts
@@ -2,6 +2,18 @@ import { DateTime } from 'luxon'
 import {BaseModel, beforeCreate, column} from '@ioc:Adonis/Lucid/Orm'
 import Generate from "../../utils/GenerateUUID";
 
+/*
+|--------------------------------------------------------------------------
+| Discord Model
+|--------------------------------------------------------------------------
+|
+| Le model Discord représente le compte Discord de l'utilisateur,
+| qui peut être relié au compte 'User'. Elle permet d'intégrer des
+| donnés disponible sur discord et ainsi créer des évènements sur discord
+| affectant le gameplay InGame.
+|
+| Author: @NathaelB
+ */
 export default class Discord extends BaseModel {
   @column({ isPrimary: true })
   public id: string
@@ -20,6 +32,8 @@ export default class Discord extends BaseModel {
   @column()
   public exp: number
 
+  @column()
+  public linked: boolean
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
--- app/Models/Minecraft.ts
+++ app/Models/Minecraft.ts
@@ -1,57 +1,44 @@
 import { DateTime } from 'luxon'
 import {BaseModel, beforeCreate, column, ManyToMany, manyToMany} from '@ioc:Adonis/Lucid/Orm'
+import Generate from "../../utils/GenerateUUID";
 import Role from "App/Models/Role";
 import Permission from "App/Models/Permission";
-import Generate from "../../utils/GenerateUUID";
-import Discord from "App/Models/Discord";
-import Minecraft from "App/Models/Minecraft";
 
 /*
 |--------------------------------------------------------------------------
-| User Model
+| Minecraft Model
 |--------------------------------------------------------------------------
 |
-| Le model User représente le compte principal que l'utilisateur
-| pourra créer via le SiteWeb
+| Le model Minecraft représente les données du joueur Minecraft.
 |
 | Author: @NathaelB
  */
-export default class User extends BaseModel {
+export default class Minecraft extends BaseModel {
   @column({ isPrimary: true })
   public id: string
 
   @beforeCreate()
-  public static async createUUID (model: User) {
+  public static async createUUID (model: Minecraft) {
     model.id = Generate.generateUUID()
   }
 
-  /**
-   * Username Minecraft:
-   * - MrNathael
-   */
+  @column()
+  public uuid: string
+
   @column()
   public username: string
-
-  @column()
-  public email: string
-
-  @column()
-  public password: string
-
-  @column()
-  public avatar: string
-
-  @column()
-  public credits: number
-
-  @column()
-  public votes: number
 
   @column()
   public banned: boolean
 
   @column()
-  public rememberMeToken: string
+  public linked: boolean
+
+  @column()
+  public coins: number
+
+  @column()
+  public duration: number
 
   @manyToMany(() => Role)
   public roles: ManyToMany<typeof Role>
@@ -59,16 +46,9 @@ export default class User extends BaseModel {
   @manyToMany(() => Permission)
   public permissions: ManyToMany<typeof Permission>
 
-  @manyToMany(() => Discord)
-  public discord: ManyToMany<typeof  Discord>
-
-  @manyToMany(() => Minecraft)
-  public minecraft: ManyToMany<typeof Minecraft>
-
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
-
 }
