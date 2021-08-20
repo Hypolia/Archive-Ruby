11d5ef7321e6745102f8f8c25fe09812b41b912e
Nathael Bonnal <kirua48000@gmail.com>
2021-08-21 00:48:17 +0200

    :construction: Update and Create model

--- app/Models/discord/Ticket.ts
+++ app/Models/discord/Ticket.ts
@@ -1,39 +1,35 @@
 import { DateTime } from 'luxon'
 import {BaseModel, beforeCreate, column} from '@ioc:Adonis/Lucid/Orm'
-import Generate from "../../utils/GenerateUUID";
+import Generate from "../../../utils/GenerateUUID";
 
 /*
 |--------------------------------------------------------------------------
-| Discord Model
+| Ticket Model
 |--------------------------------------------------------------------------
 |
-| Le model Discord représente le compte Discord de l'utilisateur,
-| qui peut être relié au compte 'User'. Elle permet d'intégrer des
-| donnés disponible sur discord et ainsi créer des évènements sur discord
-| affectant le gameplay InGame.
+| Le model Ticket permet de répertorier tout les
+| tickets en cours, et permet donc de définir une
+| limite de ticket par Utilisateur
 |
 | Author: @NathaelB
  */
-export default class Discord extends BaseModel {
+export default class Ticket extends BaseModel {
   @column({ isPrimary: true })
   public id: string
 
   @beforeCreate()
-  public static async createUUID (model: Discord) {
+  public static async createUUID (model: Ticket) {
     model.id = Generate.generateUUID()
   }
 
   @column()
-  public userId: number
+  public userId: string
 
   @column()
-  public level: number
+  public channelId: string
 
   @column()
-  public exp: number
-
-  @column()
-  public linked: boolean
+  public username: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
