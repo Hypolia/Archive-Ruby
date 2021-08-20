b18e65898a018d76c6838f0a2c441d8bdfbef146
Nathael Bonnal <kirua48000@gmail.com>
2021-08-21 00:47:10 +0200

    :construction: Create Migration

---
+++ database/migrations/1628857193870_minecrafts.ts
@@ -0,0 +1,32 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+/*
+|--------------------------------------------------------------------------
+| Minecraft Migrations
+|--------------------------------------------------------------------------
+|
+| Author: @NathaelB
+ */
+export default class Minecrafts extends BaseSchema {
+  protected tableName = 'minecrafts'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.string('id').primary()
+      table.string('uuid').unique().notNullable()
+      table.string('username').unique().notNullable()
+      table.boolean('banned')
+      table.boolean('linked')
+      table.integer('coins')
+      table.integer('duration')
+
+      table.timestamps(true, true)
+      //table.timestamp('created_at', { useTz: true })
+      //table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
