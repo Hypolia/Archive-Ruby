68ae8d23562d79cab7d895aecb7d9b6ee7ca6dd5
NathaelB <kirua48000@gmail.com>
2021-09-07 14:16:46 +0200

    :construction: Creating migration permissions for users and roles | `@ManyToMany`

---
+++ database/migrations/1630600910819_permission_roles.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class PermissionRoles extends BaseSchema {
+  protected tableName = 'permission_roles'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('permission_id').references('id').inTable('permissions')
+      table.string('role_id').references('id').inTable('roles')
+      table.unique(['role_id', 'permission_id'])
+
+      table.timestamps(true, true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
